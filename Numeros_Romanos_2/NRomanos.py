e744ea4799148f21e5c2da7d3c5f685181c0b164
Eduardo <edusaul@ific.uv.es>
2020-01-28 09:45:10 +0100

    katayunos_28_01_20

--- Numeros_Romanos_2/NRomanos.py
+++ Numeros_Romanos_2/NRomanos.py
@@ -3,7 +3,7 @@ class ArabicNumber:
     def __init__(self,number):
         self.number = number
         self.roman = ""
-        self.roman_numbers = {20:"XX", 10:"X", 9:"IX", 5:"V", 4:"IV"}
+        self.roman_numbers = {10:"X", 9:"IX", 5:"V", 4:"IV"}
 
     def to_roman(self):
         for arabic, roman in self.roman_numbers.items():
@@ -12,6 +12,6 @@ class ArabicNumber:
         return self.roman + "I"*self.number
 
     def convert(self, number, letters):
-        if self.number >= number:
+        while self.number >= number:
             self.roman += letters
             self.number -= number
